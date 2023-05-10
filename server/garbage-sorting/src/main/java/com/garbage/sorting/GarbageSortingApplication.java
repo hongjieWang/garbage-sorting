d2a2203a473095ea76cce7448da1b847f09bec44
小猿 <julywhj@163.com>
2023-05-10 22:34:06 +0800

    ti

---
+++ server/garbage-sorting/src/main/java/com/garbage/sorting/GarbageSortingApplication.java
@@ -0,0 +1,13 @@
+package com.garbage.sorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbageSortingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarbageSortingApplication.class, args);
+	}
+
+}
---
+++ server/garbage-sorting/src/test/java/com/garbage/sorting/GarbageSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garbage.sorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbageSortingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
